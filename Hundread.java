9adb85b874abe7edfb1eefb820427f76bfa62747
KaryavulaMuralikarthik <161418086+KaryavulaMuralikarthik@users.noreply.github.com>
2024-02-29 18:45:37 +0530

    Add files via upload

---
+++ Hundread.java
@@ -0,0 +1,12 @@
+public class FirstHundredEvenNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2; 
+
+        while (count < 100) {
+            System.out.println(number);
+            count++;
+            number += 2; 
+        }
+    }
+}
