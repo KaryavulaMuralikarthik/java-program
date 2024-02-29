720a223dc04a3d7438dbff57b2022278a340b8d4
KaryavulaMuralikarthik <161418086+KaryavulaMuralikarthik@users.noreply.github.com>
2024-02-29 19:37:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Loop from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            // Check if the number is a multiple of 3
+            if (i % 3 == 0) {
+                System.out.print(i + " "); 
+            }
+        }
+    }
+}
+
