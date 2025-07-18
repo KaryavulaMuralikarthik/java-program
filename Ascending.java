49912eda2e8602e5656274ceb24331a16744a4b6
KaryavulaMuralikarthik <161418086+KaryavulaMuralikarthik@users.noreply.github.com>
2025-07-18 17:58:29 +0530

    Update Ascending.java

--- Ascending.java
+++ Ascending.java
@@ -1,13 +1,8 @@
 import java.util.Arrays;
-
 public class AscendingOrder {
     public static void main(String[] args) {
-        int[] numbers = {5, 2, 8, 1, 9}; // Example numbers
-        
-        // Sort the array in ascending order
+        int[] numbers = {5, 2, 8, 1, 9}; 
         Arrays.sort(numbers);
-        
-        // Display the sorted array
         System.out.println("Numbers in ascending order:");
         for (int num : numbers) {
             System.out.print(num + " ");
