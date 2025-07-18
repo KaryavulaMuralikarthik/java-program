c8d6c36b0f80943b67fb9fab7c40a8ecd4714983
KaryavulaMuralikarthik <161418086+KaryavulaMuralikarthik@users.noreply.github.com>
2025-07-18 18:00:13 +0530

    Update Hundread.java

--- Hundread.java
+++ Hundread.java
@@ -2,7 +2,6 @@ public class FirstHundredEvenNumbers {
     public static void main(String[] args) {
         int count = 0;
         int number = 2; 
-
         while (count < 100) {
             System.out.println(number);
             count++;
